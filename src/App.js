3939893589c008dfc3267ab1a13c9c8e90896263
keldenl <keldenlin@gmail.com>
2022-04-03 13:17:57 -0700

    show oad and output data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState('');
+  const [isTranslating, setIsTranslating] = useState(false);
+  const [outputData, setOutputData] = useState();
 
   const translate = async () => {
+    setIsTranslating(true);
     fetch('http://localhost:5001/translate', {
       method: 'POST',
       headers: {
@@ -22,12 +25,22 @@ function App() {
         throw new Error(error);
       })
       .then(data => {
-        console.log(data);
+        setOutputData(data);
+        setIsTranslating(false);
+
       })
       .catch(err => {
         alert(err)
       });
   }
+
+  const outputDisplay = outputData ?
+    <div>
+      <p>{outputData.japanese}</p>
+      <p>{outputData.romanji}</p>
+      <p>{outputData.romanjiArray.join(" ")}</p>
+    </div>
+    : undefined
 
   return (
     <div className="App">
@@ -38,7 +51,14 @@ function App() {
           placeholder="Something to say"
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          disabled={isTranslating}
         />
+        <div className="loadingContainer">
+          {isTranslating ?
+            <p>Translating...</p>
+            : outputDisplay}
+          { }
+        </div>
         <button onClick={translate} className="translateButton">Translate</button>
       </div>
     </div>
