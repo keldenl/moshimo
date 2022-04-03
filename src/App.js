d4c66b0ac047f856afdc523442349f7613f37c46
keldenl <keldenlin@gmail.com>
2022-04-03 12:44:49 -0700

    simple text input implementation, with serrver side call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+
+
 function App() {
+  const [input, setInput] = useState('');
+
+  const translate = async () => {
+    fetch('http://localhost:5001/translate', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ input }),
+    })
+      .then(async (res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        const { error } = await res.json();
+        throw new Error(error);
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        alert(err)
+      });
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <textarea
+          className="inputText"
+          type="text"
+          placeholder="Something to say"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={translate} className="translateButton">Translate</button>
+      </div>
     </div>
   );
 }
